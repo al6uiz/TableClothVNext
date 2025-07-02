6143966608219d1180fa0697a55017bf5824870c
Jung Hyun, Nam <rkttu@rkttu.com>
2025-07-02 21:17:42 +0900

    설치 진행 상태 및 UI 개선
    `InstallerProgressWindowViewModel`에 디자인 타임 미리보기 메서드 추가 및 설치 단계 진행 상태와 오류 메시지 속성 구현. 비동기 설치 단계 수행 메서드 추가 및 상태를 정의하는 `StepProgress` 열거형 추가.
    
    `InstallerProgressWindow.axaml`에서 데이터 바인딩을 위한 데이터 타입 설정, 설치 단계 목록을 표시하는 `ItemsControl`로 변경, 각 단계의 진행 상태와 오류 메시지를 보여주는 UI 요소 추가, 취소 버튼을 포함하는 새로운 레이아웃 추가.

--- src/TableCloth3/Spork/ViewModels/InstallerProgressWindowViewModel.cs
+++ src/TableCloth3/Spork/ViewModels/InstallerProgressWindowViewModel.cs
@@ -1,11 +1,26 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using TableCloth3.Shared.ViewModels;
 
 namespace TableCloth3.Spork.ViewModels;
 
 public sealed partial class InstallerProgressWindowViewModel : BaseViewModel
 {
+    protected override void PrepareDesignTimePreview()
+    {
+        for (var i = 0; i < 100; i++)
+        {
+            Steps.Add(new()
+            {
+                PackageName = $"Item {i+1}",
+                PackageUrl = "https://yourtablecloth.app/",
+                PackageArguments = "/S",
+            });
+        }
+    }
+
     [ObservableProperty]
     private ObservableCollection<InstallerStepItemViewModel> _steps = [];
 }
@@ -20,4 +35,38 @@ public sealed partial class InstallerStepItemViewModel : BaseViewModel
 
     [ObservableProperty]
     private string _packageArguments = string.Empty;
+
+    [ObservableProperty]
+    private string _stepError = string.Empty;
+
+    [ObservableProperty]
+    private StepProgress _stepProgress = StepProgress.None;
+
+    [RelayCommand]
+    private async Task PerformInstallStep(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            StepProgress = StepProgress.Installing;
+
+            await Task.Delay(TimeSpan.FromSeconds(1d), cancellationToken).ConfigureAwait(false);
+
+            StepProgress = StepProgress.Succeed;
+        }
+        catch (Exception ex)
+        {
+            StepError = ex.Message;
+            StepProgress = StepProgress.Failed;
+        }
+    }
+}
+
+public enum StepProgress
+{
+    None,
+    Ready,
+    Installing,
+    Succeed,
+    Failed,
+    Unknown,
 }

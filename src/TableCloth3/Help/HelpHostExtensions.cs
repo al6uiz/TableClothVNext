314f99bcaa0ec733c5d3bb06097c42aaa0363f93
Jung Hyun, Nam <rkttu@rkttu.com>
2025-07-12 20:48:18 +0900

    헬프 모드 추가 및 뷰모델 서비스 등록
    `HelpHostExtensions.cs`에 `HelpMainWindowViewModel`과 `HelpMainWindow`를 서비스로 추가하여 의존성 주입을 설정했습니다.
    `launchSettings.json`에 `Help` 프로필을 추가하여 `--mode Help`로 애플리케이션을 실행할 수 있게 했습니다.
    새로운 `HelpMainWindowViewModel.cs` 파일을 추가하여 `BaseViewModel`을 상속받는 헬프 뷰모델 클래스를 정의했습니다.

--- src/TableCloth3/Help/HelpHostExtensions.cs
+++ src/TableCloth3/Help/HelpHostExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TableCloth3.Help.ViewModels;
 
 namespace TableCloth3.Help;
 
@@ -7,6 +8,9 @@ internal static class HelpHostExtensions
 {
     public static IHostApplicationBuilder UseTableCloth3HelpComponents(this IHostApplicationBuilder builder)
     {
+        builder.Services.AddTransient<HelpMainWindowViewModel>();
+        builder.Services.AddTransient<HelpMainWindow>();
+
         return builder;
     }
 }
---
+++ src/TableCloth3/Help/ViewModels/HelpMainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using TableCloth3.Shared.ViewModels;
+
+namespace TableCloth3.Help.ViewModels;
+
+public sealed class HelpMainWindowViewModel : BaseViewModel
+{
+    public HelpMainWindowViewModel()
+        : base()
+    {
+    }
+
+    protected override void PrepareDesignTimePreview()
+    {
+    }
+}

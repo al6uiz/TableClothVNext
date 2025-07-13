6ced2c0fd48ade3b5fab2611ce9f88def9840027
Jung Hyun, Nam <rkttu@rkttu.com>
2025-07-13 21:20:05 +0900

    메시징 기능 추가 및 MVVM 패턴 적용
    `HelpMainWindowViewModel`에서 `IMessenger`를 사용하여 메시지를 전송하고, 버튼 클릭 시 창을 닫는 기능을 구현했습니다.
    `HelpMainWindow.axaml`에서 데이터 컨텍스트를 설정하고, 버튼의 Command 속성을 `CloseButtonCommand`로 변경하여 MVVM 패턴을 준수하도록 수정했습니다.
    또한, `HelpMainWindow.axaml.cs`에서 `ICloseButtonMessageRecipient` 인터페이스를 구현하여 메시지를 수신하고, 창이 닫힐 때 메시지 등록을 해제하는 로직을 추가했습니다.

--- src/TableCloth3/Help/ViewModels/HelpMainWindowViewModel.cs
+++ src/TableCloth3/Help/ViewModels/HelpMainWindowViewModel.cs
@@ -1,15 +1,32 @@
-﻿using TableCloth3.Shared.ViewModels;
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using TableCloth3.Shared.ViewModels;
 
 namespace TableCloth3.Help.ViewModels;
 
-public sealed class HelpMainWindowViewModel : BaseViewModel
+public sealed partial class HelpMainWindowViewModel : BaseViewModel
 {
+    [ActivatorUtilitiesConstructor]
+    public HelpMainWindowViewModel(
+        IMessenger messenger)
+        : this()
+    {
+        _messenger = messenger;
+    }
+
     public HelpMainWindowViewModel()
         : base()
     {
     }
 
-    protected override void PrepareDesignTimePreview()
-    {
-    }
+    private readonly IMessenger _messenger = default!;
+
+    public sealed record class CloseButtonMessage;
+
+    public interface ICloseButtonMessageRecipient : IRecipient<CloseButtonMessage>;
+
+    [RelayCommand]
+    private void CloseButton()
+        => _messenger.Send<CloseButtonMessage>();
 }
--- src/TableCloth3/Help/Windows/HelpMainWindow.axaml.cs
+++ src/TableCloth3/Help/Windows/HelpMainWindow.axaml.cs
@@ -1,13 +1,46 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using TableCloth3.Help.ViewModels;
+using static TableCloth3.Help.ViewModels.HelpMainWindowViewModel;
 
 namespace TableCloth3;
 
-public partial class HelpMainWindow : Window
+public partial class HelpMainWindow : Window, ICloseButtonMessageRecipient
 {
+    [ActivatorUtilitiesConstructor]
+    public HelpMainWindow(
+        HelpMainWindowViewModel viewModel,
+        IMessenger messenger)
+        : this()
+    {
+        _viewModel = viewModel;
+        _messenger = messenger;
+
+        DataContext = _viewModel;
+
+        _messenger.Register<CloseButtonMessage>(this);
+    }
+
     public HelpMainWindow()
+        : base()
     {
         InitializeComponent();
+    }
+
+    private readonly HelpMainWindowViewModel _viewModel = default!;
+    private readonly IMessenger _messenger = default!;
+
+    protected override void OnClosed(EventArgs e)
+    {
+        _messenger.UnregisterAll(this);
+        base.OnClosed(e);
+    }
+
+    void IRecipient<CloseButtonMessage>.Receive(CloseButtonMessage message)
+    {
+        Close();
     }
 }
